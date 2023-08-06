067e232bf6d11ccaad3d16151e0dea94b8d33617
adityarizkyramadhan <adityarizky1020@gmail.com>
2023-08-06 08:29:42 +0700

    feat (starter) : add request city by id

--- starter.go
+++ starter.go
@@ -1,9 +1,5 @@
 package rajaongkir
 
-import (
-	"encoding/json"
-)
-
 type starter struct {
 	account *Account
 }
@@ -23,13 +19,30 @@ func (s *starter) GetProvince() ([]ProvinceResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-	jsonData, err := json.Marshal(resp.Result)
-	if err != nil {
-		return nil, err
-	}
 	var provinces []ProvinceResponse
-	if err := json.Unmarshal(jsonData, &provinces); err != nil {
+	if err := interfaceToStruct(resp.Result, &provinces); err != nil {
 		return nil, err
 	}
 	return provinces, nil
+}
+
+type CityResponse struct {
+	CityID     string `json:"city_id"`
+	ProvinceID string `json:"province_id"`
+	Province   string `json:"province"`
+	Type       string `json:"type"`
+	CityName   string `json:"city_name"`
+	PostalCode string `json:"postal_code"`
+}
+
+func (s *starter) GetCityByIDProvince(idProvince string) ([]CityResponse, error) {
+	resp, err := s.account.httpRequest("GET", s.account.typeAccount.String()+"/city?province="+idProvince, s.account.apiKey, nil)
+	if err != nil {
+		return nil, err
+	}
+	var cities []CityResponse
+	if err := interfaceToStruct(resp.Result, &cities); err != nil {
+		return nil, err
+	}
+	return cities, nil
 }
